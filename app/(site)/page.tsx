085635fb6aa228ad53978f5410fc094566dbf013
Tristan Listanco <tannyencina@gmail.com>
2023-08-12 20:10:46 +0800

    Added Search Query

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import getSongs from "@/actions/getSongs";
+import PageContent from "./components/PageContent";
 
 export const revalidate = 0;
-export default function Home() {
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div className=" bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
@@ -20,9 +23,7 @@ export default function Home() {
           </h1>
 
         </div>
-        <div>
-          List of Songs
-        </div>
+        <PageContent songs={songs}/>
       </div>
     </div>
   );
